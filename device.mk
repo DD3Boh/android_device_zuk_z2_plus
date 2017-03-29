fb3b693eef99fbe4db8858c773c6311856263f7d
offain <fabian.leutenegger@bluewin.ch>
2017-03-29 22:41:43 +0200

    gemini: import HWUI and Dalvik properties to system.prop
    Define HWUI and Dalvik properties in system_prop.mk because AOSPA dont provide the preconfigured configuration files in their framwork.
    The values i used are from 7.3.9 build and tweaked a little bit.
    
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- device.mk
+++ device.mk
@@ -69,12 +69,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# HWUI overrides
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
-
-# Dalvik overrides
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
