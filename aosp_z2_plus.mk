76afede2307017eec46134e2d7caca00c515452e
dd3boh <dade.garberi@gmail.com>
2017-08-22 12:39:21 +0200

    z2_plus: Don't call vendor/aosp
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- aosp_z2_plus.mk
+++ aosp_z2_plus.mk
@@ -15,13 +15,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
-
-# Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_NAME := aosp_z2_plus
 PRODUCT_DEVICE := z2_plus
