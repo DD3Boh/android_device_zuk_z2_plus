c693c67ecbaaefef0a176953fcc8535688164eb8
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:58 +0200

    oneplus3: Build wifilogd
    * Remove unused libqsap_sdk & libQWiFiSoftApCfg
    
    Change-Id: If3f1551d1baca9a2591297603d151a961b07edf1

--- device.mk
+++ device.mk
@@ -297,10 +297,9 @@ PRODUCT_PACKAGES += \
     ipacm \
     ipacm-diag \
     IPACM_cfg.xml \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     wificond \
+    wifilogd \
     hostapd \
     readmac \
     dhcpcd.conf \
